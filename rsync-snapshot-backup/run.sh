aa60fa001fe9007590cf16d53e48f025b6eaa566
Baptiste-Leterrier <baba.compt1@gmail.com>
2021-12-05 13:39:53 +0100

    SSH support + refacto
    Added SSH port support + refacto using sshpass best practice

--- rsync-snapshot-backup/run.sh
+++ rsync-snapshot-backup/run.sh
@@ -5,11 +5,12 @@ CONFIG_PATH=/data/options.json
 
 # parse inputs from options
 RSYNC_HOST=$(jq --raw-output ".rsync_host" $CONFIG_PATH)
-RSYNC_USER=$(jq --raw-output ".rsync_user" $CONFIG_PATH)
+SSHPASS=$(jq --raw-output ".rsync_user" $CONFIG_PATH)
 RSYNC_PASSWORD=$(jq --raw-output ".rsync_password" $CONFIG_PATH)
 REMOTE_DIRECTORY=$(jq --raw-output ".remote_directory" $CONFIG_PATH)
 SNAPSHOT_PASSWORD=$(jq --raw-output '.snapshot_password' $CONFIG_PATH)
 KEEP_LOCAL_BACKUP=$(jq --raw-output '.keep_local_backup' $CONFIG_PATH)
+SSH_PORT=$(jq --raw-output '.SSH_port' $CONFIG_PATH)
 
 echo "[INFO] Starting rsync Snapshot Backup..."
 
@@ -28,7 +29,7 @@ function create-local-backup {
 function copy-backup-to-remote {
 	rsyncurl="$RSYNC_USER@$RSYNC_HOST:$REMOTE_DIRECTORY"
 	echo "[INFO] Copying ${slug}.tar to ${REMOTE_DIRECTORY} on ${RSYNC_HOST} using rsync"
-	sshpass -p $RSYNC_PASSWORD rsync -av /backup/ $rsyncurl --ignore-existing
+	sshpass -e rsync -av -e "ssh -p ${SSH_PORT}" /backup/ $rsyncurl --ignore-existing
 }
 
 function delete-local-backup {
